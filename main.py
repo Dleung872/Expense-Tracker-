40f00e18d8fcd5ea2c292680bcfb54ef99208cc0
spoon68 <erenzbpan.68@gmail.com>
2026-01-21 13:40:31 -0800

    added a basic CLI and the addition to the list function with an exit

---
+++ main.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+expenses = []
+
+def add(name, amount):
+    expenses.append(
+        {
+        "item": name,
+        "amount": amount
+        }
+    )
+
+def main():
+    while True:
+        print('1. add\n2. see list\n3. remove\n4. exit')
+        user_input = input("> ")
+        if user_input == '1' or user_input == 'add':
+                user_item = input('item: ')
+                user_expense = input('amount: ')
+                add(user_item, user_expense)
+                print(expenses)
+                sleep(5)
+                print('\033c')
+                continue
+        
+        elif user_input == '4' or user_input == 'exit':
+            print('bye')
+            sleep(0.5)
+            print('\033c')
+            return
+main()
